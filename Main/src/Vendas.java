82c2c63a6becd8f8b4709ceca60e9e51c5044cb9
Daniel <danihard2018@gmail.com>
2025-09-15 17:59:50 -0300

    Adiciona atributos e metodo construtor

--- Main/src/Vendas.java
+++ Main/src/Vendas.java
@@ -1,3 +1,29 @@
 public class Vendas {
+    private int quantidade, tipoIngresso, tipoAplicado, idadeCliente, metodoPagamento;
+
+    private double valorTotal;
+
+    private static final double PRECO_BASE = 30.0;
+
+    private boolean ingressoGratuito;
+
+    public Vendas(int quantidade, int tipoIngresso, int idadeCliente) {
+        this.quantidade = quantidade;
+        this.tipoIngresso = tipoIngresso;
+        this.idadeCliente = idadeCliente;
+        this.ingressoGratuito = idadeCliente <= 5;
+        calcularVendas();
+    }
+    public calcularVendas(){
+        if(ingressoGratuito == true){
+            
+
+        }
+    }
+
+
+
+
+
 
 }
