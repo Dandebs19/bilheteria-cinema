f45fd4cbb488f8b1ffff2f4cde4b762b586e4bef
Paulo Queiroz <paulosamuelqb@gmail.com>
2025-09-15 19:33:13 -0300

    Add while na main

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -6,16 +6,25 @@ public class Main {
 
         System.out.println("====================BILHETERIA DE CINEMA====================");
 
-        System.out.println("Quantidade de ingressos: ");
-        int TotalIngressos = input.nextInt();
+        while (true) {
+            System.out.println("Quantidade de ingressos: ");
+            int TotalIngressos = input.nextInt();
 
-        System.out.println("Tipo de ingresso desejado: \n1 - Inteira (R$30,00) \n2 - Meia (50% Desconto) \n3 - Promocional (30% Desconto)\n4 - Gratuito (até 5 anos de idade)");
-        int TipoIngressos = input.nextInt();
+            System.out.println("Tipo de ingresso desejado: \n1 - Inteira (R$30,00) \n2 - Meia (50% Desconto) \n3 - Promocional (30% Desconto)\n4 - Gratuito (até 5 anos de idade)");
+            int TipoIngressos = input.nextInt();
 
-        Bilheteria bilheteria = new Bilheteria(TotalIngressos, TipoIngressos);
+            Bilheteria bilheteria = new Bilheteria(TotalIngressos, TipoIngressos);
 
-        System.out.printf("Ingresso comprados: %d\n", TotalIngressos);
-        bilheteria.MostrarValorCadaIngresso();
-        bilheteria.MostrarValorTotal();
+            System.out.printf("Ingresso comprados: %d\n", TotalIngressos);
+            bilheteria.MostrarValorCadaIngresso();
+            bilheteria.MostrarValorTotal();
+
+            System.out.println("Atender próximo cliente? (S/N)");
+            String AtenderProximoCliente = input.next().toUpperCase();
+
+            if (AtenderProximoCliente.equals("N")) {
+                break;
+            }
+        }
     }
 }
